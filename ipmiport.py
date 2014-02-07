50613e6873626f8dbf0b08a4b243c493b19a2d82
Talluri <kanti.sri@gmail.com>
2014-02-07 14:49:42 +0530

    add a comment
    add a comment

--- ipmiport.py
+++ ipmiport.py
@@ -2,4 +2,5 @@
 import os
 ipmi_addr = raw_input("enter the ipmi address:")
 ipmi_addr.rstrip()
+#this is a comment
 os.system("ipmitool -Uroot -Pcalvin -H%s chassis bootdev pxe;ipmitool -Uroot -Pcalvin -H%s chassis power cycle" % (ipmi_addr, ipmi_addr))
